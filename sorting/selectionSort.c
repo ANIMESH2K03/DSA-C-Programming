3b975a9a5dd4076bf813d2c5813753b25458a134
Animesh Malik <animeshmalik263@gmail.com>
2026-04-21 18:57:21 +0530

    Selection Sorting Algorithm using c programming

---
+++ sorting/selectionSort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+void display(int *,int);
+void swap(int *,int *);
+void selectionSort(int *,int);
+
+int main(){
+  int arr[] = {64, 7, 25, 12, 22, 11, 35};
+  int len = sizeof(arr)/sizeof(arr[0]);
+
+  selectionSort(arr,len);
+  display(arr,len);
+
+}
+
+void display(int *arr,int len){
+
+  for(int i=0;i<len;i++){
+    printf("%d ,",arr[i]);
+  }
+
+}
+
+void swap(int *a,int *b){
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void selectionSort(int *arr,int len){
+  if(len <= 1) return;
+
+  int min = 0;
+  for(int i=0;i<len;i++){
+    if (arr[min] > arr[i]){
+      min = i;
+    }
+  }
+  
+  swap(&arr[min],&arr[0]);
+
+  selectionSort(arr + 1,len-1);
+}
